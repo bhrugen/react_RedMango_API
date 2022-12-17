01a208959f6d906341ec2ea6981236c596637331
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-12-17 13:18:33 -0600

    Get Shopping Cart

--- RedMango_API/Controllers/ShoppingCartController.cs
+++ RedMango_API/Controllers/ShoppingCartController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Azure;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RedMango_API.Data;
@@ -18,6 +19,40 @@ namespace RedMango_API.Controllers
             _response = new();
             _db = db;
         }
+
+        [HttpGet]
+        public async Task<ActionResult<ApiResponse>> GetShoppingCart(string userId)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(userId))
+                {
+                    _response.IsSuccess = false;
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    return BadRequest(_response);
+                }
+                ShoppingCart shoppingCart = _db.ShoppingCarts
+                    .Include(u => u.CartItems).ThenInclude(u => u.MenuItem)
+                    .FirstOrDefault(u=>u.UserId==userId);
+
+                if (shoppingCart.CartItems != null && shoppingCart.CartItems.Count > 0) {
+                    shoppingCart.CartTotal = shoppingCart.CartItems.Sum(u => u.Quantity * u.MenuItem.Price);
+                }
+                _response.Result = shoppingCart;
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                     = new List<string>() { ex.ToString() };
+                _response.StatusCode = HttpStatusCode.BadRequest;
+            }
+            return _response;
+        }
+
+
 
         [HttpPost]
         public async Task<ActionResult<ApiResponse>> AddOrUpdateItemInCart(string userId, int menuItemId, int updateQuantityBy)
@@ -64,7 +99,40 @@ namespace RedMango_API.Controllers
             {
                 //shopping cart exists
 
-                //CartItem cartIteminDb = _db.CartItems.FirstOrDefault(u => u.MenuItemId == menuItemId);
+                CartItem cartItemInCart = shoppingCart.CartItems.FirstOrDefault(u => u.MenuItemId == menuItemId);
+                if(cartItemInCart == null)
+                {
+                    //item does not exist in current cart
+                    CartItem newCartItem = new()
+                    {
+                        MenuItemId = menuItemId,
+                        Quantity = updateQuantityBy,
+                        ShoppingCartId = shoppingCart.Id,
+                        MenuItem = null
+                    };
+                    _db.CartItems.Add(newCartItem);
+                    _db.SaveChanges();
+                }
+                else
+                {
+                    //item already exist in the cart and we have to update quantity
+                    int newQuantity = cartItemInCart.Quantity + updateQuantityBy;
+                    if(updateQuantityBy==0 || newQuantity <= 0)
+                    {
+                        //remove cart item from cart and if it is the only item then remove cart
+                        _db.CartItems.Remove(cartItemInCart);
+                        if (shoppingCart.CartItems.Count() == 1)
+                        {
+                            _db.ShoppingCarts.Remove(shoppingCart);
+                        }
+                        _db.SaveChanges();
+                    }
+                    else
+                    {
+                        cartItemInCart.Quantity= newQuantity;
+                        _db.SaveChanges();
+                    }
+                }
             }
             return _response;
 
