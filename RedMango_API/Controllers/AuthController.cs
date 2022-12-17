e41855ac6dc8d601f58507979df89d93e1c50e6f
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-12-17 09:50:41 -0600

    Auth Controller and Dependency Injection

---
+++ RedMango_API/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RedMango_API.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+    }
+}
