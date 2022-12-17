c9e16bf92b025af3edffb366f46253577b41002c
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-12-17 13:02:39 -0600

    Update Shopping Cart Endpoint

--- RedMango_API/Controllers/ShoppingCartController.cs
+++ RedMango_API/Controllers/ShoppingCartController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RedMango_API.Data;
+using RedMango_API.Models;
+using System.Net;
 
 namespace RedMango_API.Controllers
 {
@@ -7,5 +11,63 @@ namespace RedMango_API.Controllers
     [ApiController]
     public class ShoppingCartController : ControllerBase
     {
+        protected ApiResponse _response;
+        private readonly ApplicationDbContext _db;
+        public ShoppingCartController(ApplicationDbContext db)
+        {
+            _response = new();
+            _db = db;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ApiResponse>> AddOrUpdateItemInCart(string userId, int menuItemId, int updateQuantityBy)
+        {
+            // Shopping cart will have one entry per user id, even if a user has many items in cart.
+            // Cart items will have all the items in shopping cart for a user
+            // updatequantityby will have count by with an items quantity needs to be updated
+            // if it is -1 that means we have lower a count if it is 5 it means we have to add 5 count to existing count.
+            // if updatequantityby by is 0, item will be removed
+
+
+            // when a user adds a new item to a new shopping cart for the first time
+            // when a user adds a new item to an existing shopping cart (basically user has other items in cart)
+            // when a user updates an existing item count
+            // when a user removes an existing item
+
+            ShoppingCart shoppingCart = _db.ShoppingCarts.Include(u=>u.CartItems).FirstOrDefault(u => u.UserId == userId);
+            MenuItem menuItem = _db.MenuItems.FirstOrDefault(u => u.Id == menuItemId);
+            if(menuItem == null)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                return BadRequest(_response);
+            }
+            if(shoppingCart==null && updateQuantityBy > 0)
+            {
+                //create a shopping cart & add cart item
+
+                ShoppingCart newCart = new() { UserId = userId };
+                _db.ShoppingCarts.Add(newCart);
+                _db.SaveChanges();
+
+                CartItem newCartItem = new()
+                {
+                    MenuItemId = menuItemId,
+                    Quantity = updateQuantityBy,
+                    ShoppingCartId = newCart.Id,
+                    MenuItem=null
+                };
+                _db.CartItems.Add(newCartItem);
+                _db.SaveChanges();
+            }
+            else
+            {
+                //shopping cart exists
+
+                //CartItem cartIteminDb = _db.CartItems.FirstOrDefault(u => u.MenuItemId == menuItemId);
+            }
+            return _response;
+
+        }
     }
 }
