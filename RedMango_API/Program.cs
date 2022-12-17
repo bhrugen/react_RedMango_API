c16956c06c799539f9a131a9296e45fb724f60ae
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-12-17 15:00:27 -0600

    Deploy API

--- RedMango_API/Program.cs
+++ RedMango_API/Program.cs
@@ -86,10 +86,19 @@ builder.Services.AddSwaggerGen(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseSwagger();
+
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
     app.UseSwaggerUI();
+}
+else
+{
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseHttpsRedirection();
