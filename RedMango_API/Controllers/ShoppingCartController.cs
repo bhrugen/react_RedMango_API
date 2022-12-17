e35bfe9b686e9c31549b6b858f0c57970e595f85
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-12-17 12:39:12 -0600

    Shopping Cart Controller and Logic

---
+++ RedMango_API/Controllers/ShoppingCartController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RedMango_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShoppingCartController : ControllerBase
+    {
+    }
+}
