b57ae8e6631d08b0ccb644ea48f38ca83225d668
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-12-17 10:26:39 -0600

    JWT Token

--- RedMango_API/Controllers/AuthController.cs
+++ RedMango_API/Controllers/AuthController.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using RedMango_API.Data;
 using RedMango_API.Models;
 using RedMango_API.Models.Dto;
 using RedMango_API.Services;
 using RedMango_API.Utility;
+using System.IdentityModel.Tokens.Jwt;
 using System.Net;
+using System.Security.Claims;
+using System.Text;
 
 namespace RedMango_API.Controllers
 {
@@ -47,12 +51,29 @@ namespace RedMango_API.Controllers
             }
 
             //we have to generate JWT Token
+            var roles = await _userManager.GetRolesAsync(userFromDb);
+            JwtSecurityTokenHandler tokenHandler = new();
+            byte[] key = Encoding.ASCII.GetBytes(secretKey);
 
+            SecurityTokenDescriptor tokenDescriptor = new()
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim("fullName", userFromDb.Name),
+                    new Claim("id", userFromDb.Id.ToString()),
+                    new Claim(ClaimTypes.Email, userFromDb.UserName.ToString()),
+                    new Claim(ClaimTypes.Role, roles.FirstOrDefault()),
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
 
             LoginResponseDTO loginResponse = new()
             {
                 Email = userFromDb.Email,
-                Token = "REPLACE WITH ACTUAL TOKEN ONCE WE GENERATE"
+                Token = tokenHandler.WriteToken(token)
             };
 
             if (loginResponse.Email == null || string.IsNullOrEmpty(loginResponse.Token))
