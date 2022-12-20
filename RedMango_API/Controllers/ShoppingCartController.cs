07ead4fdd3ca480ab684386045f17346b06a7a8d
Bhrugen Patel <bhrugen1990@hotmail.com>
2022-12-20 17:33:01 -0600

    Shopping Cart API Update

--- RedMango_API/Controllers/ShoppingCartController.cs
+++ RedMango_API/Controllers/ShoppingCartController.cs
@@ -25,16 +25,18 @@ namespace RedMango_API.Controllers
         {
             try
             {
+                ShoppingCart shoppingCart;
                 if (string.IsNullOrEmpty(userId))
                 {
-                    _response.IsSuccess = false;
-                    _response.StatusCode = HttpStatusCode.BadRequest;
-                    return BadRequest(_response);
+                    shoppingCart = new();
                 }
-                ShoppingCart shoppingCart = _db.ShoppingCarts
+                else
+                {
+                    shoppingCart= _db.ShoppingCarts
                     .Include(u => u.CartItems).ThenInclude(u => u.MenuItem)
-                    .FirstOrDefault(u=>u.UserId==userId);
+                    .FirstOrDefault(u => u.UserId == userId);
 
+                }
                 if (shoppingCart.CartItems != null && shoppingCart.CartItems.Count > 0) {
                     shoppingCart.CartTotal = shoppingCart.CartItems.Sum(u => u.Quantity * u.MenuItem.Price);
                 }
